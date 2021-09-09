66fee8f255f453ba033142124ae6e41daec2dfb6
JuHyun419 <zzang9ha@naver.com>
2021-09-09 09:19:08 +0900

    initial commit

---
+++ src/test/java/com/juhyun/shorturl/ShorturlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhyun.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorturlJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
