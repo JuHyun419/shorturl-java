0260564b5818c0f73f3490d8483c39f89274ba61
JuHyun419 <zzang9ha@naver.com>
2021-09-12 16:41:26 +0900

    test: 메소드 네이밍, 더미 데이터 삽입 코드 추가

--- src/test/java/com/juhyun/shorturl/repository/ShortUrlRepositoryTest.java
+++ src/test/java/com/juhyun/shorturl/repository/ShortUrlRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.juhyun.shorturl.repository;
 
 import com.juhyun.shorturl.entity.ShortUrl;
+import com.juhyun.shorturl.util.TinyUrl;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataMongoTest
 class ShortUrlRepositoryTest {
@@ -14,16 +18,45 @@ class ShortUrlRepositoryTest {
     @Autowired
     private ShortUrlRepository shortUrlRepository;
 
+    @Disabled
     @Test
-    void test() {
+    void findTest() {
         // given
-        String existLongUrl = "http://naver.comasd@asdzcxzcxzc$%vcxxzcvxcv";
+        ShortUrl shortUrl = makeEntity();
+        shortUrlRepository.save(shortUrl);
 
         // when
-        ShortUrl newEntity = shortUrlRepository.findByLongUrl(existLongUrl).get();
+        ShortUrl newEntity = shortUrlRepository.findByLongUrl(shortUrl.getLongUrl()).get();
 
         // then
-        assertThat(existLongUrl).isEqualTo(newEntity.getLongUrl());
+        assertThat(shortUrl).isEqualTo(newEntity);
+        assertThat(shortUrl.getShortUrl()).isEqualTo(newEntity.getShortUrl());
+        assertThat(shortUrl.getLongUrl()).isEqualTo(newEntity.getLongUrl());
+    }
+
+    @Disabled
+    @Test
+    void insertDummyData() {
+        IntStream.range(0, 10000).forEach(i -> {
+            shortUrlRepository.save(makeEntity());
+        });
+    }
+
+    private ShortUrl makeEntity() {
+        Long count = shortUrlRepository.countAllDocuments();
+        long id = (count > 0)
+                ? shortUrlRepository.findMaxShortUrl().get_id() + 1
+                : 1;
+        String shortUrl = TinyUrl.idToShortUrl(id);
+        String longUrl = "http://localhost:8080/" + RandomStringUtils.randomAlphanumeric(30);
+
+        return ShortUrl.builder()
+                ._id(id)
+                .shortUrl(shortUrl)
+                .longUrl(longUrl)
+                .customUrl("")
+                .requestCount(1)
+                .build();
     }
 
 }
